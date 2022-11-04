4c7561320aaf91d308a38c6872837786807798ae
Chirag Patil <patilchirag484@gmail.com>
2022-11-04 12:58:35 -0400

    setup for Leaflet js with open street map is done

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-
-getData();
+const api_URL = `https://api.wheretheiss.at/v1/satellites/25544`;
+const map = L.map('mapContainer').setView([51.505, -0.09], 13);
+const attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
+const tileUrl = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tiles = L.tileLayer(tileUrl, { attribution });
+tiles.addTo(map);
 async function getData()
 {
-    const response = await fetch(`https://api.wheretheiss.at/v1/satellites/25544`);
+    const response = await fetch(api_URL);
     const JsonData = await response.json();
-    const data = JSON.stringify(JsonData);
-    console.log(data);
+    const { latitude, longitude, } = JsonData;
+    console.log(latitude, longitude);
+    document.getElementById('lat').innerText = latitude;
+    document.getElementById('long').innerText = longitude;
 }
+
+getData();
 
